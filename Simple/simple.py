ac149a999fe934f1c3c99d77527d5f9b8a34896a
Smail Rayane <108391279+SRayaneM@users.noreply.github.com>
2022-11-09 15:18:26 +0000

    update 2

--- Simple/simple.py
+++ Simple/simple.py
@@ -342,3 +342,36 @@ def raise_to_power(base_num, pow_num):
 
 raise_to_power(5, 2)
 '''
+#2D list.
+'''
+
+number_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]]
+
+#The first squared brackets represents the rows, while the second square brackets represents the columns.
+#print(number_grid[3][0])
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+'''
+
+
+#Translator.
+#This function will convert each vowel in the phrase to what ever letter we want it to
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+                translation = translation + "M"
+
+            else:
+
+                translation = translation + "m"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input('Enter a phrase: ')))
